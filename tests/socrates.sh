27a3b8e6379e5a22cd9e9708fed3983de80cc588
Sheela G. Khusal <sheela.khusal.08@gmail.com>
2022-08-17 21:57:26 +0200

    socrates...

--- tests/socrates.sh
+++ tests/socrates.sh
@@ -6,7 +6,7 @@
 #    By: sguilher <sguilher@student.42sp.org.br>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/08/17 21:33:34 by sguilher          #+#    #+#              #
-#    Updated: 2022/08/17 21:36:24 by sguilher         ###   ########.fr        #
+#    Updated: 2022/08/17 21:54:37 by sguilher         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,6 +15,6 @@
 cd ../
 git clone https://github.com/nesvoboda/socrates
 cd socrates
-pip3 install -r requirements.txt
+#pip3 install -r requirements.txt
 python3 socrates.py ../
 python3 delay_o_meter.py
